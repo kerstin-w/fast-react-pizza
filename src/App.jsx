fc31ada9d61ffcfa775dd4fd9d1f7ddd50e96a1b
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-18 14:09:44 +0200

    feat: Add path for all pages of the website

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +15,15 @@ const router = createBrowserRouter([
     path: "/menu",
     element: <Menu />,
   },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  { path: "/order/:orderID", element: <Order /> },
 ]);
 
 function App() {
