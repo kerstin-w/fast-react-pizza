776d54a28c94797aa8a1d3ed88a26fbad635bb6b
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-18 13:30:25 +0200

    add Cart component with mock data and navigation links
    - Create  component to display items in the user's shopping cart using mock data.
    - Include links for navigating back to the menu and proceeding to order pizzas.
    - Add a button for clearing the cart (functionality to be implemented).
    
    This component provides a user interface for viewing and managing the shopping cart.

---
+++ src/features/cart/Cart.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+const fakeCart = [
+  {
+    pizzaId: 12,
+    name: "Mediterranean",
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: "Vegetale",
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: "Spinach and Mushroom",
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
+
+/**
+ * The Cart component renders a user's shopping cart with options to order pizzas or clear the cart.
+ * @returns The Cart component is being returned, which contains JSX elements for displaying the cart
+ * contents and options for ordering pizzas or clearing the cart.
+ */
+function Cart() {
+  const cart = fakeCart;
+
+  return (
+    <div>
+      <Link to="/menu">&larr; Back to menu</Link>
+
+      <h2>Your cart, %NAME%</h2>
+
+      <div>
+        <Link to="/order/new">Order pizzas</Link>
+        <button>Clear cart</button>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
