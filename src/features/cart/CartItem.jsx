2db5b34f997b9290ea8b906f621225641ba444ab
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-19 19:22:32 +0200

    style: Add styling to Cart and CartItem

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,4 +1,5 @@
-import { formatCurrency } from "../../utils";
+import Button from "../../ui/Button";
+import { formatCurrency } from "../../utilities/helpers";
 
 /**
  * The `CartItem` function renders a list item displaying details of a pizza item
@@ -10,12 +11,13 @@ function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li>
-      <p>
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">🗑️ Delete</Button>
       </div>
     </li>
   );
