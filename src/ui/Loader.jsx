b4df7dea9be95701b546e52cdf979b263f4fd2ca
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-19 11:34:09 +0200

    feat: add loading indicator to AppLayout with useNavigation hook
    - Utilize  hook in  to track navigation state.
    - Define  to determine if the current state is loading.
    - Render a  component conditionally based on .
    - Implement  component to display a loading indicator.
    
    This enhancement improves user experience by providing a visual indication when data is being fetched or a page is loading.

---
+++ src/ui/Loader.jsx
@@ -0,0 +1,9 @@
+/**
+ * The Loader function returns a div element with the class name "loader".
+ * @returns A JSX element representing a loader with the class name "loader" is being returned.
+ */
+function Loader() {
+  return <div className="loader"></div>;
+}
+
+export default Loader;
