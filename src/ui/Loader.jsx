63a5603f8eb095e1eb10ecaa70864520f728de0a
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-19 18:26:08 +0200

    feat: Adjust Loader component with backdrop and spinner
    - Implement  component to display a loading spinner.
    - Style the loader with absolute positioning to cover the entire viewport.
    - Add a semi-transparent background and a blur effect to indicate loading state.
    - Center the spinner within the loader using flexbox.
    
    This component enhances user experience by providing a visual indication when content is being loaded.

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,9 +1,13 @@
 /**
- * The Loader function returns a div element with the class name "loader".
- * @returns A JSX element representing a loader with the class name "loader" is being returned.
+ * The Loader function returns an element that displays a loader centered on the screen.
+ * @returns A JSX element is being returned. It consists of a `div` element with the class names "absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm" and contains another `div` element with the class name "loader". This structure is used to display a loading spinner or animation on the screen.
  */
 function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default Loader;
