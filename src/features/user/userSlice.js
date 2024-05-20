1bcfb30fe3f5158939e12b4ab160f6a08fdbbc90
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-20 14:05:39 +0200

    feat: add geolocation logic to fetch user's address in userSlice
    - Implemented asynchronous thunk  to fetch user's address based on geolocation.
    - Utilized  and  functions to obtain user's coordinates and reverse geocode into an address.
    - Updated the  state to include fields for position, address, and error handling.
    - Added extra reducers to handle pending, fulfilled, and rejected states of the  thunk.

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,34 +1,41 @@
-/*
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+import { getAddress } from "../../services/apiGeocoding";
+
 function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 }
 
-async function fetchAddress() {
-  // 1) We get the user's geolocation position
-  const positionObj = await getPosition();
-  const position = {
-    latitude: positionObj.coords.latitude,
-    longitude: positionObj.coords.longitude,
-  };
+export const fetchAddress = createAsyncThunk(
+  "user/fetchAddress",
+  async function () {
+    // 1) We get the user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
 
-  // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-  const addressObj = await getAddress(position);
-  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+    // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
-  // 3) Then we return an object with the data that we are interested in
-  return { position, address };
-}
-*/
-
-import { createSlice } from "@reduxjs/toolkit";
+    // 3) Then we return an object with the data that we are interested in
+    return { position, address };
+  }
+);
 
 /** The `const initialState = { username: "" };` line is defining the initial state for the user slice in a Redux store. In Redux, the initial state represents the initial values of the state managed by the reducer. 
 In this case, the initial state for the user slice includes a property `username` with an initial value of an empty string. This initial state will be used as the starting point for the user slice's state before any actions are dispatched to update it. 
 */
 const initialState = {
   username: "",
+  status: "idle",
+  position: {},
+  address: "",
+  error: "",
 };
 
 /** This code snippet is creating a slice for managing user-related state in a Redux store using the `createSlice` function from the `@reduxjs/toolkit` package.*/
@@ -40,6 +47,20 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.status = "idle";
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      }),
 });
 
 export const { updateName } = userSlice.actions;
