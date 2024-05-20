9c16878a7f3ce3b592a3ce691f86d3f852af2a8a
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-20 15:46:19 +0200

    feat: fetching data from Menu with useFetcher. Pass in the ingeredients from the Menu into the Order

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,6 +1,7 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useEffect } from "react";
+import { useFetcher, useLoaderData } from "react-router-dom";
 
 import { getOrder } from "../../services/apiRestaurant";
 import {
@@ -12,6 +13,16 @@ import OrderItem from "./OrderItem";
 
 function Order() {
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === "idle") fetcher.load("/menu");
+    },
+    [fetcher]
+  );
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -53,7 +64,15 @@ function Order() {
       </div>
       <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === "loading"}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
